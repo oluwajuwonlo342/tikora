b5b0c3523275a79a845c29cf280f9e4f8327a98d
Awoloro Daniel <106816238+oluwajuwonlo342@users.noreply.github.com>
2026-09-18 20:35:05 +0100

    Update eventService.js

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/events';
+const API_URL = 'https://tikora-backend.onrender.com/api/api/events';
 
 export const getAllEvents = async () => {
   // We do not need the auth header here because this is a public route
